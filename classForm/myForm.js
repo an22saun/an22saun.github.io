1455e20996fdb6f6b4533b801d17b4d32fafd41d
an22saun <an22saun@siena.edu>
2026-02-24 09:21:16 -0500

    edits

--- classForm/myForm.js
+++ classForm/myForm.js
@@ -1,5 +1,13 @@
-<script>
-    let a = "hello";
-    console.log("Hello World!");
-    alert("Hello World");
-</script>
+
+let a = "hello";
+console.log("Hello World!");
+alert("Hello World");
+
+const buttonA = document.querySelector('#button_A');
+const headingA =document.getElementById("Heading_A");
+
+buttonA.addEventListener('click', function(){
+const name = prompt("Wht is your name?");
+alert('Hello ${name}, nice to see you!');
+headingA.textContent = 'welcome ${name}';
+});
