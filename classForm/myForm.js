8893a8002c7aca1bf287bbb2035c3a79cd36c8b4
Aiyanna <an22saun@siena.edu>
2026-02-24 09:12:26 -0500

    edits

--- classForm/myForm.js
+++ classForm/myForm.js
@@ -1,5 +1,12 @@
-<script>
-    let a = "hello";
-    console.log("Hello World!");
-    alert("Hello World");
-</script>
+
+let a = "hello";
+console.log("Hello World!");
+alert("Hello World");
+
+const buttonA = document.querySelector(#heading);
+const headingA = 
+
+buttonA.a
+const name = prompt("Wht is your name?");
+alert('Hello ${name}, nice to see you!');
+
