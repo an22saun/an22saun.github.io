2a5439e85c0aa336214364981f2fa098ad672a33
an22saun <an22saun@siena.edu>
2026-02-25 12:21:16 -0500

    Finished lab 5

--- numberGame/numberGame.js
+++ numberGame/numberGame.js
@@ -9,34 +9,35 @@ let guessCount = 1;
 let resetButton;
 
 //functions
-function checkGuess(){
+function checkGuess() {
     const userGuess = Number(guessField.value);
-    if(guessCount === 1){
-        guesses.textContent = "Previous guesses:";
+
+    if (guessCount === 1) {
+        guesses.textContent = 'Previous guesses: ';
     }
+
     guesses.textContent = `${guesses.textContent} ${userGuess}`;
 
-    if(userGuess === randomNumber){
-        lastResult.textContent = "Congratulations! You got it right!";
-        lastResult.style.backgroundColor = "green";
-        lowOrHi.textContent = "";
+    if (userGuess === randomNumber) {
+        lastResult.textContent = 'Congratulations! You got it right!';
+        lastResult.className = 'lastResult alert alert-success';
+        lowOrHi.textContent = '';
         setGameOver();
-    }
-    else if(guessCount === 10){
-        lastResult.textContent = "!!!GAMEOVER!!!";
-        lowOrHi.textContent = "";
-        setGameOver();
-    }
-    else{
-        lastResult.textContent = "Wrong!";
-        lastResult.style.backgroundColor = "red";
-        if(userGuess < randomNumber){
-            lowOrHi.textContent = "Last guess was too low!";
-        }
-        else if(userGuess > randomNumber) {
-            lowOrHi.textContent = "Last guess was too high!";
-        }
 
+    } else if (guessCount === 10) {
+        lastResult.textContent = '!!!GAME OVER!!!';
+        lastResult.className = 'lastResult alert alert-danger';
+        lowOrHi.textContent = '';
+        setGameOver();
+
+    } else {
+        lastResult.textContent = 'Wrong!';
+        lastResult.className = 'lastResult alert alert-danger';
+        if (userGuess < randomNumber) {
+            lowOrHi.textContent = 'Last guess was too low!';
+        } else if (userGuess > randomNumber) {
+            lowOrHi.textContent = 'Last guess was too high!';
+        }
     }
 
     guessCount++;
@@ -46,19 +47,20 @@ function checkGuess(){
 
 guessSubmit.addEventListener("click", checkGuess);
 
-function setGameOver(){
+function setGameOver() {
     guessField.disabled = true;
     guessSubmit.disabled = true;
     resetButton = document.createElement("button");
     resetButton.textContent = "Start new game";
+    resetButton.className = 'btn btn-success mt-3';
     document.body.append(resetButton);
     resetButton.addEventListener("click", resetGame);
 }
 
-function resetGame(){
+function resetGame() {
     guessCount = 1;
     const resetParas = document.querySelectorAll(".resultParas p");
-    for(const resetPara of resetParas){
+    for (const resetPara of resetParas) {
         resetPara.textContent = "";
     }
 
@@ -69,6 +71,6 @@ function resetGame(){
     guessField.value = "";
     guessField.focus();
 
-    lastResult.style.backgroundColor = "white";
+    lastResult.className = 'lastResult';
     randomNumber = Math.floor(Math.random() * 100) + 1;
 }
