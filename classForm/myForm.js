1bfdc92d8966ba62cdd4f1daebd70aaa883a4ffa
an22saun <an22saun@siena.edu>
2026-02-25 12:05:50 -0500

    Lab 5

--- classForm/myForm.js
+++ classForm/myForm.js
@@ -3,11 +3,28 @@ let a = "hello";
 console.log("Hello World!");
 alert("Hello World");
 
+//slide 11
 const buttonA = document.querySelector('#button_A');
 const headingA =document.getElementById("Heading_A");
 
 buttonA.addEventListener('click', function(){
-const name = prompt("Wht is your name?");
-alert('Hello ${name}, nice to see you!');
-headingA.textContent = 'welcome ${name}';
+const name = prompt("What is your name?");
+alert(`Hello ${name}, nice to see you!`);
+headingA.textContent = 'Welcome ${name}';
 });
+
+//slide 14
+const textBox = document.querySelector("#textBox");
+const output = document.querySelector("#output");
+textBox.addEventListener("keydown", (event) => {
+    output.textContent = `You pressed "${event.key}".`;
+});
+
+//slide 20
+const button = document.querySelector("button");
+function greet(){
+    const name = prompt("What is your name?");
+    const greeting = document.querySelector("#greeting");
+    greeting.textContent = `Hello ${name}, nice to see you!`;
+}
+button.addEventListener("click", greet);
