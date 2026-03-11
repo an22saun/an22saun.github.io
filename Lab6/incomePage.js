900b61477a82fcf36add23518449928f8e860521
an22saun <an22saun@siena.edu>
2026-03-11 12:22:47 -0400

    continuing lab 6

---
+++ Lab6/incomePage.js
@@ -0,0 +1,17 @@
+document.getElementById("incomeForm").addEventListener("submit", function (e){
+    e.preventDefault();
+    
+    //Get values
+    const name = document.getElementById("sourceName").value;
+    const amount = document.getElementById("amount").value;
+    const frequency = document.getElementById("frequency").value;
+
+    //Convert amount into decimal, rounded 2 places
+    let n = parseInt(amount).toFixed(2);
+
+    //Log to console
+    console.log("Form submitted Successfully!");
+    console.log("Name:", name);
+    console.log("Amount:", n);
+    console.log("Frequency:", frequency);
+});
